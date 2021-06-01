b25d449eda8a428de5348bf2b5d5878df8cf651e
Phongfuckboi <1851061367@e.tlu.edu.vn>
2021-06-01 17:45:54 +0700

    new

--- app/src/main/java/com/example/photocalendar_app1/export_calendar.java
+++ app/src/main/java/com/example/photocalendar_app1/export_calendar.java
@@ -1,5 +1,6 @@
 package com.example.photocalendar_app1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -14,7 +15,9 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +27,8 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -32,6 +37,9 @@ public class export_calendar extends AppCompatActivity {
 
 
     private ImageView img, img_user;
+    private ImageView img_cam , img_gall;
+    private final static int CAMERA_REQUEST_CODE = 1;
+    private final static int GALLERLY_REQUEST_CODE = 11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,37 +71,75 @@ public class export_calendar extends AppCompatActivity {
            }
        }
 
+       //get image form cam
+        img_cam= findViewById(R.id.img_camara);
+        pickimagecam();
 
-       //get  image_user
-        String filename1 = getIntent().getStringExtra("img_user_c");
-        String filename2 = getIntent().getStringExtra("img_user_g");
-        Log.d("AAA",""+filename2);
-        if(filename1!=null) {
-            try {
-                FileInputStream is = this.openFileInput(filename);
-                bmp = BitmapFactory.decodeStream(is);
-                img_user.setImageBitmap(bmp);
-                is.close();
+        //get from gallary
+        img_gall= findViewById(R.id.img_gallerly);
+        pickimagegallerly();
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        else if(filename2!=null)
-        {
-            try {
-                FileInputStream is = this.openFileInput(filename1);
-                bmp = BitmapFactory.decodeStream(is);
-                is.close();
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+
 
 
     }
 
+    private void pickimagegallerly() {
+        img_gall.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                Intent intent_gallerly = new Intent(Intent.ACTION_PICK,  android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent_gallerly, GALLERLY_REQUEST_CODE);
+            }
+        });
+    }
+
+    private void pickimagecam() {
+        img_cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        if ( requestCode == GALLERLY_REQUEST_CODE) {
+
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    Log.d("AAA","hihi"+bitmap);
+                    img_user.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        else if(requestCode== CAMERA_REQUEST_CODE)
+        {
+
+            try {
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                img_user.setImageBitmap(thumbnail);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 //    public void draw(Canvas canvas){
 //        Bitmap original = getBitmapFromAsset(this,"anh1.jpg");
 //        Bitmap mask =getBitmapFromAsset(this,"ntitle.png");
